e47e6adb7a08114e048f1fdcd7c1126536597af6
netstorm <netstorm@netstorms-MacBook-Pro.local>
2020-12-23 15:30:51 +0530

    Support different delimiters

--- add.java
+++ add.java
@@ -2,8 +2,15 @@ class Test{
 
   int Add(String numbers){
     int sum = 0;
+    String deli = "\\n|,";
     if(numbers.length() > 0){
-      String arr[] = numbers.split("\\n|,");
+      // check if first character is number then proced else change delimeter value
+      if(Character.getNumericValue(numbers.charAt(0)) < 0){
+        String ar[] = numbers.split("\n");
+        deli = ar[0];
+        numbers = ar[1];
+      }
+      String arr[] = numbers.split(deli);
       int len = arr.length;
       try{
         if(Character.getNumericValue(numbers.charAt(numbers.length()-1)) < 0) throw new NumberFormatException();
@@ -27,5 +34,8 @@ class Test{
     System.out.println("TestCase 2(\"1\n3,4\n9\"): "+t.Add("1\n3,4\n9"));
     System.out.println("TestCase 3(\"1\n\n3,4\n9\"): "+t.Add("1\n\n3,4\n9"));
     System.out.println("TestCase 4(\"1,9\n0\n\"): "+t.Add("1,9\n0\n"));
+    System.out.println("TestCase 5(\"::\n1::9::0::\"): "+t.Add("::\n1::9::0::"));
+    System.out.println("TestCase 6(\"]\n1]9]0\"): "+t.Add("]\n1]9]0"));
+    System.out.println("TestCase 7(\"@\n1@9@0\"): "+t.Add("@\n1@9@0"));
   }
 }
