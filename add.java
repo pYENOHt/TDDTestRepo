4ba67258b5fc8c9fc08b48eb0e36ae8e625744b4
netstorm <netstorm@netstorms-MacBook-Pro.local>
2020-12-25 18:29:53 +0530

    done all 9 tasks

--- add.java
+++ add.java
@@ -2,19 +2,35 @@ class Test{
 
   int Add(String numbers){
     int sum = 0;
+   // default delimeter
     String deli = "\\n|,";
+   
     if(numbers.length() > 0){
-      if(numbers.charAt(0)!='-' && Character.getNumericValue(numbers.charAt(0)) < 0){
+      char charAtInd0 = numbers.charAt(0);
+      // modify delimeter if required.
+      if(charAtInd0 != '-' && Character.getNumericValue(charAtInd0) < 0){
         String ar[] = numbers.split("\n");
-        deli = ar[0];
+
+        // mulitple delimeter handling
+        if(charAtInd0 == '['){
+          deli = "";
+          String delimSet[] = ar[0].substring(1, ar[0].length()-1).split("\\]\\[");
+          for(int i = 0; i< delimSet.length; i++)
+            deli += delimSet[i]+"|";
+          
+          deli = deli.substring(0,deli.length()-1);
+        } else 
+          deli = ar[0];
         numbers = ar[1];
       }
       String arr[] = numbers.split(deli);
       int len = arr.length;
       try{
+        // check if string format is invalid (e.g, “1,\n”)
         if(Character.getNumericValue(numbers.charAt(numbers.length()-1)) < 0) throw new NumberFormatException("invalid numbers, please pass a valid string");
         if(len == 1){
           sum = Integer.parseInt(arr[0]);
+          if(sum < 1001) sum = 0;
           if(sum < 0) throw new NumberFormatException("Negatives not allowed & you passed "+sum);
         } else {
           String ngNumber = "";
@@ -24,7 +40,8 @@ class Test{
               ngNumber += arr[i] + " ";
               continue;
             }
-            sum += num;
+            if(num < 1001)
+              sum += num;
           }
           if(ngNumber.length() > 0) throw new NumberFormatException("Negatives not allowed & you passed "+ngNumber);
         }
@@ -46,6 +63,10 @@ class Test{
     System.out.println("TestCase 6(\"]\n1]9]0\"): "+t.Add("]\n1]9]0"));
     System.out.println("TestCase 7(\"@\n1@9@0\"): "+t.Add("@\n1@9@0"));
     System.out.println("TestCase 8(\"@\n-1@-9@-0\"): "+t.Add("@\n-1@-9@-0"));
+    System.out.println("TestCase 9(\"[@][!]\n1!9@-0\"): "+t.Add("[@][!]\n1!9@-0"));
+    System.out.println("TestCase 10(\"[@!]\n1!@9\"): "+t.Add("[@!]\n1@!9"));
+    System.out.println("TestCase 11(\"1000\n9\"): "+t.Add("1000\n9"));
+    System.out.println("TestCase 12(\"1,3,4\"): "+t.Add("1,3,4") );
   }
 }
 
